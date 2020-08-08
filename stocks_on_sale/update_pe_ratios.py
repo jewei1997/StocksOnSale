17405f46b7988c705334d02651a58adf59af19b4
Jeremy Wei <jeremy.wei@flatiron.com>
2020-08-07 21:28:00 -0700

    Saved all pe ratios to db

---
+++ stocks_on_sale/update_pe_ratios.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from stocks.models import Stock
+
+count = 0
+
+
+def get_pe(ticker):
+    global count
+    if count > 0 and count % 3 == 0:
+        print("Sleeping for 5 seconds")
+        time.sleep(5)
+    count += 1
+    yhoo_finance_html = requests.get('https://finance.yahoo.com/quote/' + ticker)
+    soup = BeautifulSoup(yhoo_finance_html.content)
+    try:
+        pe_span = soup.find_all("td", attrs={"data-test": "PE_RATIO-value"})[0]
+    except:
+        print(f"Unable to get pe ratio for {ticker}")
+        return float("nan")
+    try:
+        pe = float(pe_span.contents[0].contents[0])
+    except:
+        print(f"{ticker} has pe ratio of NA")
+        return float("nan")
+    print(f"{count}: {ticker} - {pe}")
+    return pe
+
+
+stocks = Stock.objects.all()
+tickers = [stock.ticker for stock in stocks]
+for ticker in tickers:
+    pe = get_pe(ticker)
+    # save new pe into db
+    s = Stock.objects.get(ticker=ticker)
+    s.pe_ratio = pe
+    print(f"Saving Stock {s} into db")
+    s.save()
+
+
