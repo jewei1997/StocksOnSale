224e8a3813b99eaa3ea2b35cfce9abcf95ffd003
Jeremy Wei <jeremy.wei@flatiron.com>
2020-08-09 22:07:24 -0700

    Trying to populate prod postgres db with tickers and pe ratios

---
+++ scrape_tickers.py
@@ -0,0 +1,16 @@
+from stocks.models import Stock
+
+with open("SnP500.txt", 'r') as reader:
+    line = reader.readline()	
+    count = 0	
+    while line != '':	
+        line = reader.readline()	
+        if "external text" in line and not "sec.gov" in line:	
+            line = line[:len(line)-5]	
+            last_carrot_idx = -1	
+            while line[last_carrot_idx] != '>':	
+                last_carrot_idx-=1	
+            ticker = line[last_carrot_idx+1:]
+            s = Stock(ticker=ticker)
+            s.save()
+            count += 1
--- stocks_on_sale/settings.py
+++ stocks_on_sale/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'stocks_on_sale.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES = {}
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 
 # Password validation
