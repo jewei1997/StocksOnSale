de9b894c7ca92a9b80ac9179c54b9ef5668b0326
Jeremy Wei <jeremy.wei@flatiron.com>
2020-09-11 00:21:35 -0700

    Added percentage changes to Stock model

--- stocks/models.py
+++ stocks/models.py
@@ -5,6 +5,12 @@ class Stock(models.Model):
     ticker = models.CharField(max_length=10, primary_key=True)
     pe_ratio = models.FloatField(blank=True, null=True)
     market_cap = models.IntegerField()
+    one_week_percentage_change = models.FloatField(blank=True, null=True)
+    one_month_percentage_change = models.FloatField(blank=True, null=True)
+    one_year_percentage_change = models.FloatField(blank=True, null=True)
 
     def __str__(self):
-        return f"{self.ticker}, PE: {self.pe_ratio}, MC: {self.market_cap}"
+        return f"{self.ticker}, PE: {self.pe_ratio}, MC: {self.market_cap}, " \
+               f"Week % delta: {self.one_week_percentage_change}, " \
+               f"Month % delta: {self.one_month_percentage_change}, " \
+               f"Year % delta: {self.one_year_percentage_change}"
